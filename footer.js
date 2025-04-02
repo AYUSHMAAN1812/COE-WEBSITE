78cb3c51b27596ab73b8eb7ce2cac526e7e93bc9
AYUSHMAAN1812 <ayushmaan2005dav@gmail.com>
2025-04-02 09:18:23 +0530

    Updated footer

--- footer.js
+++ footer.js
@@ -1,11 +1,13 @@
 class Footer extends HTMLElement {
     connectedCallback() {
-        this.innerHTML =
-            `
-        <p class="footer d-flex align-items-center">&copy; Department of COE, IIT Hyderabad. All rights reserved.</p>
+        this.innerHTML = `
+        <div class="blockquote-footer text-center mb-0">
+        <p>&copy; Department of COE, IIT Hyderabad. All rights reserved.</p>
         <p>Last Updated: <span id="lastUpdated"></span></p>
+        </div>
         `;
-        // this.querySelector("#lastUpdated").textContent = document.lastModified;
+        this.querySelector("#lastUpdated").textContent = document.lastModified;
     }
 }
+
 customElements.define('my-footer', Footer);
