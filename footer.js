1687bc75d03d0b5683f9e0b467574053cd5d2ee6
anushree200 <aanuushree@gmail.com>
2025-06-07 22:00:10 +0530

    added video

--- footer.js
+++ footer.js
@@ -1,9 +1,13 @@
 class Footer extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
-        <div class="blockquote-footer text-center mb-0">
-        <p>&copy; Department of COE, IIT Hyderabad. All rights reserved.</p>
-        <p>Last Updated: <span id="lastUpdated"></span></p>
+        <div class="blockquote-footer text-center mb-0" style="background-color: #0c1a2b; color: #ffffff; width: 100vw; position: relative; left: 50%; right: 50%; margin-left: -50vw; margin-right: -50vw; padding: 20px 0;">
+            <p style="font-size:30px;">Check out the Video</p>
+            <div style="max-width: 560px; margin: 20px auto;">
+                <iframe width="100%" height="315" src="https://www.youtube.com/embed/WotHf4J8kiQ?mute=1&loop=1&playlist=WotHf4J8kiQ" title="YouTube video player"></iframe>
+            </div>
+            <p>© Department of COE, IIT Hyderabad. All rights reserved.</p>
+            <p>Last Updated: <span id="lastUpdated"></span></p>
         </div>
         `;
         this.querySelector("#lastUpdated").textContent = document.lastModified;
